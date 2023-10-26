dbc5c1e1d76843cd09a3f3ffc3d0c64f1a8986cd
teman67 <>
2023-10-26 09:09:40 +0000

    practicing rest operator

--- arrow-functions.js
+++ arrow-functions.js
@@ -42,3 +42,18 @@ console.log(obj2)
 
 let obj3={...obj1, b:6}
 console.log(obj3)
+
+
+/* rest operator */
+
+const sumRest = (a,b,c,...rest) => {
+    let sum = a + b + c;
+    for (let i of rest){
+        sum += i;
+    }
+
+    return sum
+}
+
+let sum = sumRest (1,2,3,4,5,6);
+console.log(sum)
