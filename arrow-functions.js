888eef9a1d9976868eb71950e1a1ca1ef9dc6565
teman67 <>
2023-10-26 08:57:12 +0000

    practicing arrow function and spread operator

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,41 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers (a,b) {
+    return a+b
+}
 
+result=addTwoNumbers(4,5)
+console.log(result)
 // Arrow Function With Parameters
+
+const addTwoNumbers2 = (a,b) => a+b;
+result2=addTwoNumbers2(5,7)
+console.log(result2)
 
 // Single Line Arrow Function With Parameters
 
 // Implicit Returns
 
 // Returning Multiple Lines
+
+
+let arr1=[1,2,3];
+let arr2=arr1;
+arr2.push(4)
+console.log(arr2)
+console.log(arr1)
+
+let arr3=[4,5,6];
+arr4=[...arr3];
+arr4.push(7);
+console.log(arr3)
+console.log(arr4)
+
+let obj1={a:1, b:2, c:3}
+let obj2={...obj1, d:4}
+console.log(obj1)
+console.log(obj2)
+
+let obj3={...obj1, b:6}
+console.log(obj3)
